f9a0ebd0777341b6b50e23c225cc9fd5e2ccca4e
Wolfgang Jeltsch <wolfgang@well-typed.com>
2023-07-16 20:10:36 +0300

    Improve the layout of the `imports` clause
    This complements #71.

--- src/Equivalence_Reasoner-Usage_Example.thy
+++ src/Equivalence_Reasoner-Usage_Example.thy
@@ -3,9 +3,9 @@ text_raw \<open>\appendix\<close>
 section \<open>Usage Example\<close>
 
 theory "Equivalence_Reasoner-Usage_Example"
-imports
-  Equivalence_Reasoner
-  "HOL-Analysis.Extended_Real_Limits"
+  imports
+    Equivalence_Reasoner
+    "HOL-Analysis.Extended_Real_Limits"
 begin
 
 subsection \<open>Extended Reals\<close>
